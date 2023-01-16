d7ef7c3ef329af4efa29d3dbff82e914d10a3b6f
LivailleDev <livaille.dev@gmail.com>
2023-01-16 00:40:55 +0000

    InstalandoMaven

---
+++ PrimisComanda/src/main/java/com/hulxon/primis/PrimisApplication.java
@@ -0,0 +1,13 @@
+package com.hulxon.primis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimisApplication.class, args);
+	}
+
+}
---
+++ PrimisComanda/src/test/java/com/hulxon/primis/PrimisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hulxon.primis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
