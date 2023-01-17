21fb700e5121caafaad11716d01596c5f8350481
LivailleDev <livaille.dev@gmail.com>
2023-01-17 00:36:02 +0000

    Preparacao SpringBoot JAVA

--- primiscomanda/src/main/java/com/hulxon/primiscomanda/PrimiscomandaApplication.java
+++ primiscomanda/src/main/java/com/hulxon/primiscomanda/PrimiscomandaApplication.java
@@ -1,13 +1,13 @@
-package com.hulxon.primis;
+package com.hulxon.primiscomanda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PrimisApplication {
+public class PrimiscomandaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PrimisApplication.class, args);
+		SpringApplication.run(PrimiscomandaApplication.class, args);
 	}
 
 }
--- primiscomanda/src/test/java/com/hulxon/primiscomanda/PrimiscomandaApplicationTests.java
+++ primiscomanda/src/test/java/com/hulxon/primiscomanda/PrimiscomandaApplicationTests.java
@@ -1,10 +1,10 @@
-package com.hulxon.primis;
+package com.hulxon.primiscomanda;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PrimisApplicationTests {
+class PrimiscomandaApplicationTests {
 
 	@Test
 	void contextLoads() {
