4b5c8e33198910cab4dc4600e52accb26766726b
LivailleDev <livaille.dev@gmail.com>
2023-03-14 16:14:18 +0000

    Mudança do componente Table e algumas tentativas de incluir no storybook

--- primisds/src/components/Table.tsx
+++ primisds/src/components/Table.tsx
@@ -2,62 +2,66 @@ import React, { useState } from 'react';
 
 function ProductTable() {
     const [products, setProducts] = useState([
-        { id: 1, descricao: 'vodka', unidade: 'un' , precodecompra: '5', precodevenda: '60', quantidade:'700', editar: "", excluir: "" },
-        { id: 2, descricao: 'agua', unidade: 'un' , precodecompra: '1', precodevenda: '5', quantidade:'500', editar: "", excluir: "" },
-        { id: 3, descricao: 'cerveja', unidade: 'un' , precodecompra: '2', precodevenda: '7', quantidade:'200', editar: "", excluir: "" },
-        
+        { id: 1, descricao: 'vodka', unidade: 'un', precodecompra: '5', precodevenda: '60', quantidade: '700', editar: "", excluir: "" },
+        { id: 2, descricao: 'agua', unidade: 'un', precodecompra: '1', precodevenda: '5', quantidade: '500', editar: "", excluir: "" },
+        { id: 3, descricao: 'cerveja', unidade: 'un', precodecompra: '2', precodevenda: '7', quantidade: '200', editar: "", excluir: "" },
+
     ]);
     const [query, setQuery] = useState('');
 
-
-    /*function handleQueryChange(event) {
-        setQuery(event.target.value);
-    }*/
-
-    function addProduct() {
-        const newProduct = { id: 4, descricao: 'coca-cola', unidade: 'un' , precodecompra: '1', precodevenda: '7', quantidade:'200', editar: "", excluir: "" }
-        setProducts([...products, newProduct]);
-      }
-
-     
-/*    const filteredProducts = products.filter((product) =>
-        product.name.toLowerCase().includes(query.toLowerCase())
-    ); */
+    /*
+        function handleQueryChange(event) {
+            setQuery(event.target.value);
+        }*/
+    interface Props {
+        onSearch: (value: string) => void;
+    }
 
 
-    return (
-        <div>
-            <input type="text" value={query} onChange={handleQueryChange} />
-            <table>
-                <thead>
-                    <tr>
-                        <th>DESCRIÇÃO</th>
-                        <th>UNIDADE</th>
-                        <th>PREÇO DE COMPRA</th>
-                        <th>PREÇO DE VENDA</th>
-                        <th>QUANTIDADE</th>
-                        <th>EDITAR</th>
-                        <th>EXCLUIR</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {products.map(product => (
-                        <tr key={product.id}>
-                            <td>{product.descricao}</td>
-                            <td>{product.unidade}</td>
-                            <td>{product.precodecompra}</td>
-                            <td>{product.precodevenda}</td>
-                            <td>{product.quantidade}</td>
-                            <td>{product.editar}</td>
-                            <td>{product.excluir}</td>
 
+        function addProduct() {
+            const newProduct = { id: 4, descricao: 'coca-cola', unidade: 'un', precodecompra: '1', precodevenda: '7', quantidade: '200', editar: "", excluir: "" }
+            setProducts([...products, newProduct]);
+        }
+
+        /*
+      const filteredProducts = products.filter((product) =>
+           product.name.toLowerCase().includes(query.toLowerCase())
+       ); */
+
+
+        return (
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>DESCRIÇÃO</th>
+                            <th>UNIDADE</th>
+                            <th>PREÇO DE COMPRA</th>
+                            <th>PREÇO DE VENDA</th>
+                            <th>QUANTIDADE</th>
+                            <th>EDITAR</th>
+                            <th>EXCLUIR</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
-            <button onClick={() => addProduct()}>Adcionar Produto</button>
-        </div>
-    );
-}
+                    </thead>
+                    <tbody>
+                        {products.map(product => (
+                            <tr key={product.id}>
+                                <td>{product.descricao}</td>
+                                <td>{product.unidade}</td>
+                                <td>{product.precodecompra}</td>
+                                <td>{product.precodevenda}</td>
+                                <td>{product.quantidade}</td>
+                                <td>{product.editar}</td>
+                                <td>{product.excluir}</td>
 
-export default ProductTable;
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+                <button onClick={() => addProduct()}>Adcionar Produto</button>
+            </div>
+        );
+    }
+
+    export default ProductTable;
